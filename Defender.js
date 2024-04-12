67f4425b55921a339e8febbb3e2b1b9c279b71e1
Athrva Deshmukh <112002659+athrvadeshmukh@users.noreply.github.com>
2024-04-12 14:11:09 +0530

    Create Defender.js

---
+++ Defender.js
@@ -0,0 +1,58 @@
+layout('us');
+// REM - Adding a comment (REM statement)
+// This is a comment line
+// REM - Adding a comment (REM statement)
+// This is a comment line
+// REM - Adding a comment (REM statement)
+// This is a comment line
+// REM - Adding a comment (REM statement)
+// This is a comment line
+// REM - Adding a comment (REM statement)
+// This is a comment line
+delay(1000);
+press("GUI ");
+delay(500);
+type("virus & threat protection");
+delay(1000);
+press("ENTER");
+delay(3000);
+press("TAB");
+delay(1000);
+press("TAB");
+delay(1000);
+press("TAB");
+delay(1000);
+press("TAB");
+delay(200);
+press("ENTER");
+delay(200);
+press("SPACE");
+delay(3000);
+press("ALT y");
+delay(1000);
+press("TAB");
+delay(200);
+delay(1000);
+press("TAB");
+delay(200);
+press("TAB");
+delay(200);
+press("ENTER");
+delay(200);
+press("SPACE");
+delay(1000);
+press("TAB");
+delay(200);
+press("ENTER");
+delay(200);
+press("SPACE");
+delay(1000);
+press("TAB");
+delay(1000);
+press("TAB");
+delay(200);
+press("ENTER");
+delay(200);
+press("SPACE");
+delay(500);
+press("GUI D");
