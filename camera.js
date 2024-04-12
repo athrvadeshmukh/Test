d6b0f0a278a586289e1a12aeb6aa8b161e490ae8
Athrva Deshmukh <112002659+athrvadeshmukh@users.noreply.github.com>
2024-04-12 14:07:23 +0530

    Create camera.js

---
+++ camera.js
@@ -0,0 +1,26 @@
+layout('us');		// US keyboard layout
+typingSpeed(100,150)	// Wait 100ms between key strokes + an additional random value between 0ms and 150ms (natural)
+//waitLEDRepeat(NUM);		// Wait till NUM LED of target changes frequently multiple times (doesn't work on OSX)
+press("GUI r");
+delay(1000);
+type("microsoft.windows.camera:\n")
+delay(1000);
+press("TAB")
+delay(1000);
+press("ENTER")
+delay(200);
+press("ENTER")
+delay(200);
+press("ENTER")
+delay(200);
+press("ENTER")
+delay(1000);
+press("UP")
+delay(1000);
+press("ENTER")
+delay(30000);
+press("ENTER")
+delay(1000)
+press("DOWN")
+delay(100);
+press("ALT F4")
